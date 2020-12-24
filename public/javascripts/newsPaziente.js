8b8b9b003e8c76fede58c24c1c8ceb47c55344ca
Vinziman <mikele454@gmail.com>
2020-12-24 01:50:16 +0100

    Grafica importata

--- public/javascripts/newsPaziente.js
+++ public/javascripts/newsPaziente.js
@@ -1,86 +1,86 @@
-const newsContainerElement = document.querySelector('.newsContainer');
-const form = document.querySelector('#cercaNewsForm');
-const searchInput = document.querySelector('#cercaInput');
-const errorElement = document.querySelector('.error-message');
-const loadMoreElement = document.querySelector('#loadMore');
-const getNewsAPI_URL = 'http://localhost:3000/comunicazioni/loadComunicazioni';
+const newsContainerElement = document.querySelector(".newsContainer");
+const form = document.querySelector("#cercaNewsForm");
+const searchInput = document.querySelector("#cercaInput");
+const errorElement = document.querySelector(".error-message");
+const loadMoreElement = document.querySelector("#loadMore");
+const getNewsAPI_URL = "http://localhost:3000/comunicazioni/loadComunicazioni";
 
 let skip = 0;
 let limit = 5;
 let loading = false;
 let finished = false;
-let search = '';
+let search = "";
 
-document.addEventListener('readystatechange', (event) => {
-	if (event.target.readyState === 'complete') {
-		initPage();
-	}
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initPage();
+  }
 });
 
 const initPage = () => {
-	form.reset();
-	listAllNews();
+  form.reset();
+  listAllNews();
 };
 
-document.addEventListener('scroll', () => {
-	const rect = loadMoreElement.getBoundingClientRect();
-	if (rect.top < window.innerHeight && !loading && !finished) {
-		loadMore();
-	}
+document.addEventListener("scroll", () => {
+  const rect = loadMoreElement.getBoundingClientRect();
+  if (rect.top < window.innerHeight && !loading && !finished) {
+    loadMore();
+  }
 });
 
-form.addEventListener('submit', (event) => {
-	event.preventDefault();
-	listAllNews();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  listAllNews();
 });
 
 function loadMore() {
-	skip += limit;
-	listAllNews(false);
+  skip += limit;
+  listAllNews(false);
 }
 
 function listAllNews(reset = true) {
-	loading = true;
-	if (reset) {
-		skip = 0;
-		finished = false;
-		search = searchInput.value;
-		newsContainerElement.innerHTML = '';
-	}
-	if (!finished)
-		fetch(`${getNewsAPI_URL}?search=${search}&skip=${skip}&limit=${limit}`)
-			.then((response) => response.json())
-			.then((listaComunicazioni) => {
-				listaComunicazioni.comunicazioni.forEach((comunicazione) => {
-					const div = document.createElement('div');
+  loading = true;
+  if (reset) {
+    skip = 0;
+    finished = false;
+    search = searchInput.value;
+    newsContainerElement.innerHTML = "";
+  }
+  if (!finished)
+    fetch(`${getNewsAPI_URL}?search=${search}&skip=${skip}&limit=${limit}`)
+      .then((response) => response.json())
+      .then((listaComunicazioni) => {
+        listaComunicazioni.comunicazioni.forEach((comunicazione) => {
+          const div = document.createElement("div");
 
-					const header = document.createElement('h3');
-					header.textContent = comunicazione.titolo;
+          const header = document.createElement("h3");
+          header.textContent = comunicazione.titolo;
 
-					const contents = document.createElement('p');
-					contents.textContent = comunicazione.testo;
+          const contents = document.createElement("p");
+          contents.textContent = comunicazione.testo;
 
-					const date = document.createElement('small');
-					date.textContent = comunicazione.data;
+          const date = document.createElement("small");
+          date.textContent = comunicazione.data;
 
-					const link = document.createElement('p');
-					link.innerHTML = `<a href="/comunicazioni/${comunicazione._id}">view more</a>`;
+          const link = document.createElement("p");
+          link.innerHTML = `<a href="/comunicazioni/${comunicazione._id}">view more</a>`;
 
-					div.appendChild(header);
-					div.appendChild(contents);
-					div.appendChild(date);
-					div.appendChild(link);
-
-					newsContainerElement.appendChild(div);
-					loading = false;
-				});
-				if (!listaComunicazioni.meta.has_more) {
-					loadMoreElement.style.visibility = 'hidden';
-					finished = true;
-				} else {
-					loadMoreElement.style.visibility = 'visible';
-				}
-			});
+          div.appendChild(header);
+          div.appendChild(contents);
+          div.appendChild(date);
+          div.appendChild(link);
+          div.setAttribute("class", "comunicazioni");
+          newsContainerElement.appendChild(div);
+          loading = false;
+        });
+        if (!listaComunicazioni.meta.has_more) {
+          loadMoreElement.style.visibility = "hidden";
+          finished = true;
+        } else {
+          loadMoreElement.style.visibility = "visible";
+        }
+      });
 }
 
 /* function convertDate(d) {
