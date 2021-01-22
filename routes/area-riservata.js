2619713eef3ee0dddf68cb853657d92b243b134e
Vinziman <mikele454@gmail.com>
2021-01-22 22:55:01 +0100

    Modifiche

--- routes/area-riservata.js
+++ routes/area-riservata.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const { asyncErrorHandler, isUserLogged } = require('../middleware');
+const {
+	asyncErrorHandler,
+	isUserLogged,
+	isStaffMember,
+} = require('../middleware');
 const { getPrenotazioni } = require('../controllers/area-riservata-medico');
 
 /* GET index /area-riservata */
-router.get('/', isUserLogged, (req,res,next)=>{
-    res.send('AREA RISERVATA');
+router.get('/', isUserLogged, isStaffMember, (req, res, next) => {
+	res.send('AREA RISERVATA');
 });
 
 /* GET index /area-riservata/medico/prenotazioni */
 router.get('/medico/prenotazioni', asyncErrorHandler(getPrenotazioni));
+
+/* GET index /area-riservata/prova */
+router.get('/prova', (req, res, next) => {
+	const orari = [
+		{ ora: '10:15', disponibile: true },
+		{ ora: '10:30', disponibile: false },
+	];
+	res.render('../views/area-riservata/segreteria/aggiungi-prenotazione', {
+		title: 'Prova - HAP',
+		orari,
+	});
+});
 
 module.exports = router;
