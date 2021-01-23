d69fb9b5a52e879a967920b04215d84ca167c21c
Phil <filippo.troiani96@gmail.com>
2021-01-23 18:02:35 +0100

    lista pazienti medico backend

--- routes/area-riservata.js
+++ routes/area-riservata.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { asyncErrorHandler, isUserLogged, isStaffMember } = require('../middleware');
-const { indexAreaRiservata, getPrenotazioni } = require('../controllers/area-riservata');
+const { indexAreaRiservata, getPrenotazioniMedico, getPazientiMedico } = require('../controllers/area-riservata');
 
 /* GET index /area-riservata */
 router.get('/', isUserLogged, isStaffMember, indexAreaRiservata);
 
 /* GET index /area-riservata/medico/prenotazioni */
-router.get('/medico/prenotazioni', asyncErrorHandler(getPrenotazioni));
+router.get('/medico/prenotazioni', isUserLogged, isStaffMember, asyncErrorHandler(getPrenotazioniMedico));
+
+/* GET index /area-riservata/medico/lista-pazienti */
+router.get('/medico/lista-pazienti', isUserLogged, isStaffMember, asyncErrorHandler(getPazientiMedico));
 
 /* GET index /area-riservata/prova */
 router.get('/prova', (req, res, next) => {
