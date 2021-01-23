f483b700c3fb2595743a6ddf01b4de383be01e80
Phil <filippo.troiani96@gmail.com>
2021-01-23 17:45:01 +0100

    medico show prenotazioni

--- controllers/area-riservata.js
+++ controllers/area-riservata.js
@@ -6,26 +6,25 @@ module.exports={
         else res.redirect('/');
     },
     async getPrenotazioni(req,res,next){
-        if(typeof req.body.data=='undefined') {
+        if(typeof req.query.data=='undefined') {
             var data=new Date();
-            data.setHours(0);
+            data.setHours(3);
             data.setMinutes(0);
         }else{
-            var dataRicevuta=req.body.data.split('-'); 
-            var data=new Date(`${dataRicevuta[2]}-${dataRicevuta[1]}-${dataRicevuta[0]}T00:00:00`);
+            var dataRicevuta=req.query.data.split('-'); 
+            var data=new Date(`${req.query.data}T03:00:00`)//`${dataRicevuta[2]}-${dataRicevuta[1]}-${dataRicevuta[0]}T03:00:00`);
         }
         var data2=new Date(data);
         data2.setDate(data2.getDate()+1);
         const prenotazioniRisultati =await Prenotazione.find({
             medico: req.user.idRef._id,
             dataPrenotazione:{$gte:data,$lt:data2}
-        });
+        }).populate('paziente');
         var prenotazioni = prenotazioniRisultati.map((prenotazione) => {
 			var tmpPrenotazione = prenotazione.toObject();
-			tmpPrenotazione.ora = `${tmpPrenotazione.dataPrenotazione.getHours()}:${tmpPrenotazione.dataPrenotazione.getMinutes()}`;
+			tmpPrenotazione.ora = `${(tmpPrenotazione.dataPrenotazione.getHours() < 10 ? '0' : '') + tmpPrenotazione.dataPrenotazione.getHours()}:${(tmpPrenotazione.dataPrenotazione.getMinutes() < 10 ? '0' : '') + tmpPrenotazione.dataPrenotazione.getMinutes()}`;
 			return tmpPrenotazione;
-		});
-        console.log(prenotazioni);
-        res.status(302).send('OK.');
+        });
+        res.render('area-riservata/medico/prenotazioni',{title:'Prenotazioni - HAP', prenotazioni,data});
     }
 };
